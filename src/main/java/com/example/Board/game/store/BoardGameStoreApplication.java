3abc349dba16dc38caaac151d3fb97db5eff4b0f
Mason <mason@Masons-MacBook-Air-2.local>
2024-10-24 14:46:01 +0100

    first commit

---
+++ src/main/java/com/example/Board/game/store/BoardGameStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.Board.game.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Board/game/store/BoardGameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Board.game.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
